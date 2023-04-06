1b14799ff37335bf95339a7601b6c77c5b550688
jackconwayiv <jackconwayiv@gmail.com>
2023-04-06 06:46:11 -0700

    updated weather endpoint

--- routes/weather.js
+++ routes/weather.js
@@ -7,24 +7,54 @@ const config = {
   },
 };
 
-//get - split this into two endpoints, one with params and one without (for my local weather)
-router.get("/:latitude,:longitude", async (req, res, next) => {
+router.get("", async (req, res, next) => {
   try {
-    // const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-    // console.log("========", ip);
-    // console.log(req.socket.remoteAddress);
+    let ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    console.log("========", ip);
+    console.log(req.socket.remoteAddress);
+    if (ip === "::1") {
+      ip = process.env.LOCAL_IP;
+    }
     const ipApi = await axios.get(`http://ip-api.com/json/`); //https://ip-api.com/docs/api:json
     const lat = ipApi.data.lat;
     const lon = ipApi.data.lon;
-    // console.log("lat: ", lat, " lon: ", lon);
+    console.log("lat: ", lat, " lon: ", lon);
     const stationResponse = await axios.get(
       `https://api.weather.gov/points/${lat},${lon}`,
       config,
     );
-    // const stationResponse = await axios.get(
-    //   `https://api.weather.gov/points/${req.params.latitude},${req.params.longitude}`,
-    //   config,
-    // );
+    const grid = stationResponse.data.properties;
+    const gridId = grid.gridId; //may need cwa instead of gridId
+    const gridX = grid.gridX;
+    const gridY = grid.gridY;
+    console.log(grid.timeZone);
+    const city = grid.relativeLocation.properties.city;
+    const state = grid.relativeLocation.properties.state;
+
+    const forecast = await axios.get(
+      `https://api.weather.gov/gridpoints/${gridId}/${parseInt(
+        gridX,
+      )},${parseInt(gridY)}/forecast`,
+      config,
+    );
+    console.log(`second request done`);
+    const weatherBundle = {
+      city: city,
+      state: state,
+      forecast: forecast.data,
+    };
+    res.send(weatherBundle);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:latitude,:longitude", async (req, res, next) => {
+  try {
+    const stationResponse = await axios.get(
+      `https://api.weather.gov/points/${req.params.latitude},${req.params.longitude}`,
+      config,
+    );
     const grid = stationResponse.data.properties;
     const gridId = grid.gridId; //may need cwa instead of gridId
     const gridX = grid.gridX;
@@ -52,6 +82,3 @@ router.get("/:latitude,:longitude", async (req, res, next) => {
 });
 
 module.exports = router;
-
-//https://api.weather.gov/points/33.5268,-112.0844
-//https://api.weather.gov/zones/forecast/AZZ544
